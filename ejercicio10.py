ed58abed0769ae50388fc0ae7bd993f9284b4e70
ivansaucedo088 <ivansaucedo088@gmail.com>
2024-05-24 08:34:11 -0600

    Programa 10 - Sumas

---
+++ ejercicio10.py
@@ -0,0 +1,12 @@
+while True:
+    n1 = 0
+    n2 = 0
+    print("sumador de numeros")
+
+    n1 = int(input("dame un nunemro "))
+    n2 = int(input("dame tu segundo numero "))
+
+    print(n1 + n2)
+
+    if n1 or n2 == "n":
+        break
