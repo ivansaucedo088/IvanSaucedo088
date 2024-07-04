79420645787712225790719607f639c563f7c0a9
ivansaucedo088 <ivansaucedo088@gmail.com>
2024-07-04 12:39:14 -0600

    Programa 24 – Clases y Objetos - Agregación

--- practicas ana/clases y objetos/programa24.py
+++ practicas ana/clases y objetos/programa24.py
@@ -1,66 +1,66 @@
-Clase  Coche :
-    def  __init__ ( self , marca , modelo , año ):
-        yo mismo . marca  =  marca
-        yo mismo.modelo = modelo
-        yo mismo.año = año  
-        self . en_marcha  =  Falso
+class Coche:
+    def __init__(self, marca, modelo, año):
+        self.marca = marca
+        self.modelo = modelo
+        self.año = año
+        self.en_marcha = False
 
-    def  arrancar ( yo mismo ):
+    def arrancar(self):
         self.en_marcha = True
-        print ( "El coche" , self.marca , self.modelo , " ha arrancado . " )
+        print("El coche", self.marca, self.modelo, "ha arrancado.")
 
-    def  detener ( self ):
-        self . en_marcha  =  Falso
-        print ( "El coche" , self . marca , self . modelo , "se ha detenido." )
+    def detener(self):
+        self.en_marcha = False
+        print("El coche", self.marca, self.modelo, "se ha detenido.")
 
-    def  mostrar_info ( self ):
-        print ( "Marca:" , self.marca , " Modelo :" , self.modelo , " Año:" , self.año , " En marcha :" , ' Sí' if self.en_marcha else " No" )    
+    def mostrar_info(self):
+        print("Marca:", self.marca, "Modelo:", self.modelo, "Año:", self.año, "En marcha:" , 'Sí' if self.en_marcha else "No")
 
-clase  Dueño :
-    def  __init__ ( self , nombre , teléfono ):
-        yo mismo.nombre = nombre
-        yo mismo . teléfono  =  teléfono
-        yo mismo . carros  = []
+class Dueño:
+    def __init__(self, nombre, telefono):
+        self.nombre = nombre
+        self.telefono = telefono
+        self.carros = []
 
-    def  comprar_carros ( self , carro ):
-        ser . coches . añadir ( carro )
-        print ( self.nombre , " Teléfono : " , self.telefono , " ha comprado un " , carro.marca , carro.modelo )
+    def comprar_carros(self, carro):
+        self.carros.append(carro)
+        print(self.nombre, "Teléfono:", self.telefono, "ha comprado un", carro.marca, carro.modelo)
 
-    def  vender_carros ( self , carro ):
-        si  carro  en  si mismo . carros :
-            ser . coches . quitar ( carro )
-            print ( self.nombre , " está vendiendo un " , carro.marca , carro.modelo )
-        demás :
-            print ( self.nombre , " no tiene un " , carro.marca , carro.modelo , " para vender " )
+    def vender_carros(self, carro):
+        if carro in self.carros:
+            self.carros.remove(carro)
+            print(self.nombre, "está vendiendo un", carro.marca, carro.modelo)
+        else:
+            print(self.nombre, "no tiene un", carro.marca, carro.modelo, "para vender")
 
-    def  alquilar_carros ( self , carro ):
-        si  carro  en  si mismo . carros :
-            print ( self.nombre , " está rentando un " , carro.marca , carro.modelo )
-        demás :
-            print ( self.nombre , " no tiene uno " , carro.marca , carro.modelo , " para alquilar " )
+    def rentar_carros(self, carro):
+        if carro in self.carros:
+            print(self.nombre, "está rentando un", carro.marca, carro.modelo)
+        else:
+            print(self.nombre, "no tiene un", carro.marca, carro.modelo, "para rentar")
 
 # Crear dos objetos a partir de la clase Coche
-coche1  =  Coche ( "Toyota" , "Corolla" , 2020 )
-coche2  =  Coche ( "Honda" , "Civic" , 2018 )
+coche1 = Coche("Toyota", "Corolla", 2020)
+coche2 = Coche("Honda", "Civic", 2018)
 
 # Crear tres objetos a partir de la clase Dueño
-carro1  =  Dueño ( "Carlos" , "61282625262" )
-carro2  =  Dueño ( "Angel" , "61485423262" )
-carro3  =  Dueño ( "Yamir" , "61527282636" )
+carro1 = Dueño("Carlos", "61282625262")
+carro2 = Dueño("Angel", "61485423262")
+carro3 = Dueño("Yamir", "61527282636")
 
-#Usar algunos métodos y métodos
-coche1 . mostrar_info ()
-coche1 .arrancar ( )
-coche1 . detener ()
+# Usar algunos atributos y métodos
+coche1.mostrar_info()
+coche1.arrancar()
+coche1.detener()
 
-coche2 . mostrar_info ()
-coche2 .arrancar ( )
-coche2 . detener ()
+coche2.mostrar_info()
+coche2.arrancar()
+coche2.detener()
 
-# Primero, los dueños deben comprar los coches antes de poder venderlos o alquilarlos.
-carro2 . comprar_carros ( coche1 )
-carro3 . comprar_carros ( coche2 )
+# Primero, los dueños deben comprar los coches antes de poder venderlos o rentarlos
+carro2.comprar_carros(coche1)
+carro3.comprar_carros(coche2)
 
-# Ahora pueden vender o alquilar
-carro2 . vender_carros ( coche1 )
-carro3 . alquilar_carros ( coche2 )
+# Ahora pueden vender o rentar
+carro2.vender_carros(coche1)
+carro3.rentar_carros(coche2)
