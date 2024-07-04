98161fe1fcc545e2db51b8ddf75b8040dbbc7f79
ivansaucedo088 <ivansaucedo088@gmail.com>
2024-07-04 12:38:45 -0600

    Programa 23 – Clases y Objetos

--- practicas ana/clases y objetos/programa23.py
+++ practicas ana/clases y objetos/programa23.py
@@ -1,32 +1,32 @@
-Clase  Coche :
-    def  __init__ ( self , marca , modelo , año ):
-        yo mismo . marca  =  marca
-        yo mismo.modelo = model 
-        yo mismo.año = año  
-        self . en_marcha  =  Falso
-
-    def  arrancar ( yo mismo ):
+class Coche:
+    def __init__(self, marca, modelo, año):
+        self.marca = marca
+        self.modelo = modelo
+        self.año = año
+        self.en_marcha = False
+    
+    def arrancar(self):
         self.en_marcha = True
-        print ( "El coche" , self.marca , self.modelo , " ha arrancado . " )
-
-    def  detener ( self ):
-        self . en_marcha  =  Falso
-        print ( "El coche" , self . marca , self . modelo , "se ha detenido." )
-
-    def  mostrar_info ( self ):
-        print ( "Marca:" , self.marca , " Modelo:" , self.modelo , " Año : " , self.año , " En marcha:" , 'Sí' if self.en_marcha else ' No ' , )    
+        print("El coche", self.marca, self.modelo, "ha arrancado.")
+    
+    def detener(self):
+        self.en_marcha = False
+        print("El coche", self.marca, self.modelo, "se ha detenido.")
+    
+    def mostrar_info(self):
+        print("Marca:", self.marca, "Modelo:", self.modelo, "Año:", self.año, "En marcha:", 'Sí' if self.en_marcha else 'No',)
 
 # Crear dos objetos a partir de la clase Coche
-coche1  =  Coche ( "Toyota" , "Corolla" , 2020 )
-coche2  =  Coche ( "Honda" , "Civic" , 2018 )
+coche1 = Coche("Toyota", "Corolla", 2020)
+coche2 = Coche("Honda", "Civic", 2018)
 
-#Usar algunos métodos y métodos
-coche1 . mostrar_info ()
-coche1 .arrancar ( )
-coche1 . detener ()
+# Usar algunos atributos y métodos
+coche1.mostrar_info()
+coche1.arrancar()
+coche1.detener()
 
 
 
-coche2 . mostrar_info ()
-coche2 .arrancar ( )
-coche2 . detener ()
+coche2.mostrar_info()
+coche2.arrancar()
+coche2.detener()
